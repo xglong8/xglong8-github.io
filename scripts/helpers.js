5484d17610d8b9aa6206294dab912d53e22e8dae
xlong <longxiaogang@163.com>
2019-09-22 15:06:47 +0800

    Change the theme

--- scripts/helpers.js
+++
@@ -1,9 +0,0 @@
-const joinPath = require('./utils/join-path');
-
-hexo.extend.helper.register('css_ex', function (base, relative) {
-  return this.css(joinPath(base, relative));
-});
-
-hexo.extend.helper.register('js_ex', function (base, relative) {
-  return this.js(joinPath(base, relative));
-});
